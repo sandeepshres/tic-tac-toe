4e2adabf350e69b200b7e5795e0e6793ced586da
Sandeep Shrestha <sandeep.c.shrestha@gmail.com>
2019-11-03 22:43:19 -0600

    added game page and routing

---
+++ src/app/game/game.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-game',
+  templateUrl: './game.page.html',
+  styleUrls: ['./game.page.scss'],
+})
+export class GamePage implements OnInit {
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
