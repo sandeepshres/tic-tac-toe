aec05394319086d75b78cc80eba15045777a82ce
Sandeep Shrestha <sandeep.c.shrestha@gmail.com>
2019-11-05 23:51:15 -0600

    rules ovbservable test

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { RulesService } from '../providers/rules.service';
+import { Rules } from '../shared/rules';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -8,7 +11,13 @@ import { Router } from '@angular/router';
 })
 export class HomePage {
 
-  constructor(private router: Router) { }
+  private rules: Observable<Rules[]>;
+  constructor(private router: Router, private ruleService: RulesService) { }
+
+  ngOnInit() {
+    this.rules = this.ruleService.getRules();
+  }
+
   goToGamePage() {
     this.router.navigate(['game']);
   }
